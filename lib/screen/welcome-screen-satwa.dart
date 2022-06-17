e2697203a1ab89e49994f671dfc623a27a20b9bb
Firdaus Nuur Rhamadhan <firdausnuurr@gmail.com>
2022-06-17 23:44:15 +0700

    Scroll fixed

--- lib/screen/welcome-screen-satwa.dart
+++ lib/screen/welcome-screen-satwa.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
 
 class WelcomeScreenSatwa extends StatelessWidget {
+  const WelcomeScreenSatwa({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Satwa'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Text('Welcome to Satwa'),
-          ],
-        ),
+    return DefaultTextStyle(
+      style: Theme.of(context).textTheme.bodyText2!,
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints viewportConstraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: viewportConstraints.maxHeight,
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Container(
+                    // A fixed-height child.
+                    color: const Color(0xffeeee00), // Yellow
+                    height: 420.0,
+                    alignment: Alignment.center,
+                    child: const Text('Fixed Height Content'),
+                  ),
+                  Container(
+                    // Another fixed-height child.
+                    color: const Color(0xff008000), // Green
+                    height: 820.0,
+                    alignment: Alignment.center,
+                    child: const Text('Fixed Height Content'),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
