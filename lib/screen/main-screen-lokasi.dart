78f8f73d98fa3d9967f91cd290543a837d9cef87
Firdaus Nuur Rhamadhan <firdausnuurr@gmail.com>
2022-06-19 00:00:51 +0700

    Whell  ScrollView Succes

--- lib/screen/main-screen-lokasi.dart
+++ lib/screen/main-screen-lokasi.dart
@@ -1,47 +1,78 @@
 import 'package:flutter/material.dart';
 
 class MainScreenLokasi extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return DefaultTextStyle(
-      style: Theme.of(context).textTheme.bodyText2!,
-      child: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints viewportConstraints) {
-          return Scaffold(
-            body: SingleChildScrollView(
-              child: ConstrainedBox(
-                constraints: BoxConstraints(
-                  minHeight: viewportConstraints.maxHeight,
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    Container(
-                      // A fixed-height child.
-                      color: Color.fromARGB(255, 35, 196, 142), // Yellow
-                      height: 420.0,
-                      alignment: Alignment.center,
-                      child: const Text('Fixed Height Content'),
-                    ),
-                    Container(
-                      // Another fixed-height child.
-                      color: Color.fromARGB(255, 200, 57, 236), // Green
-                      height: 820.0,
-                      alignment: Alignment.center,
-                      child: const Text('Fixed Height Content'),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            floatingActionButton: FloatingActionButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-          );
-        },
+    return Scaffold(
+      body: Center(
+        child: ListWheelScrollView(
+          itemExtent: 75,
+          children: items,
+        ),
       ),
     );
   }
 }
+
+List<Widget> items = [
+  ListTile(
+    leading: Icon(Icons.local_activity, size: 50),
+    title: Text('Activity'),
+    subtitle: Text('Description here'),
+  ),
+  ListTile(
+    leading: Icon(Icons.local_airport, size: 50),
+    title: Text('Airport'),
+    subtitle: Text('Description here'),
+  ),
+  ListTile(
+    leading: Icon(Icons.local_atm, size: 50),
+    title: Text('ATM'),
+    subtitle: Text('Description here'),
+  ),
+  ListTile(
+    leading: Icon(Icons.local_bar, size: 50),
+    title: Text('Bar'),
+    subtitle: Text('Description here'),
+  ),
+  ListTile(
+    leading: Icon(Icons.local_cafe, size: 50),
+    title: Text('Cafe'),
+    subtitle: Text('Description here'),
+  ),
+  ListTile(
+    leading: Icon(Icons.local_car_wash, size: 50),
+    title: Text('Car Wash'),
+    subtitle: Text('Description here'),
+  ),
+  ListTile(
+    leading: Icon(Icons.local_convenience_store, size: 50),
+    title: Text('Heart Shaker'),
+    subtitle: Text('Description here'),
+  ),
+  ListTile(
+    leading: Icon(Icons.local_dining, size: 50),
+    title: Text('Dining'),
+    subtitle: Text('Description here'),
+  ),
+  ListTile(
+    leading: Icon(Icons.local_drink, size: 50),
+    title: Text('Drink'),
+    subtitle: Text('Description here'),
+  ),
+  ListTile(
+    leading: Icon(Icons.local_florist, size: 50),
+    title: Text('Florist'),
+    subtitle: Text('Description here'),
+  ),
+  ListTile(
+    leading: Icon(Icons.local_gas_station, size: 50),
+    title: Text('Gas Station'),
+    subtitle: Text('Description here'),
+  ),
+  ListTile(
+    leading: Icon(Icons.local_grocery_store, size: 50),
+    title: Text('Grocery Store'),
+    subtitle: Text('Description here'),
+  ),
+];
